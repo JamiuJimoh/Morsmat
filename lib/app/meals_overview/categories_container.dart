cd9a4d2f43aec6ef236630a292ca17da5f531385
Janiu <hyperjay55@gmail.com>
2021-04-21 19:08:46 +0100

    initial commit of the meals_overview_page

---
+++ lib/app/meals_overview/categories_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../common_widgets/custom_container.dart';
+import '../../constants.dart';
+
+class CategoriesContainer extends CustomContainer {
+  CategoriesContainer()
+      : super(
+          height: 90.0,
+          width: 90.0,
+          child: Container(),
+          // containerColor: kScaffoldColor,
+          // borderColor: kAccentColor,
+        );
+}
